80cb38812d3c157c119ece73fad6b741c8c6ec12
Gmust <4orgood@gmail.com>
2023-06-01 16:08:24 +0200

    Setting up app

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { cva } from 'class-variance-authority';
+import { Button } from '@/components/shared/Button';
+
+interface dashboardProps {
+
+}
+
+
+
+
+const Dashboard = ({}: dashboardProps) => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+
+export default Dashboard;

b9d5200a54e9e3dd9b20056a152a0f2717db8a5d
Gmust <4orgood@gmail.com>
2023-06-05 19:51:28 +0200

    Created adding and denying friends full

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { fetchRedis } from '@/helpers/redis';
+import { db } from '@/lib/db';
 
 export async function POST(req: Request) {
   try {
@@ -28,10 +29,17 @@ export async function POST(req: Request) {
       return new Response('No friend request', { status: 400 });
     }
 
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd);
+    await db.sadd(`user:${idToAdd}:friends`, session.user.id);
 
+    //await db.srem(`user:${idToAdd}:outbound_friend_requests`, session.user.id);
+    await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
 
     return new Response('OK');
   } catch (e) {
-
+    if (e instanceof z.ZodError) {
+      return new Response('Invalid request payload', { status: 422 });
+    }
+    return new Response('Invalid request', { status: 400 });
   }
 }
